f09d0dade7003526baa82f1e0a4f59868b88b5bb
Boris Leistedt <boris.leistedt@gmail.com>
2020-10-03 21:48:38 +0100

    added simple relations between flux and magnitudes

---
+++ gasp/photometry.py
@@ -0,0 +1,21 @@
+def flux2mag(fluxes, mag_zeropoint=0):
+    """
+    Flux to magnitude convertion
+    """
+    return -2.5 * np.log10(fluxes) - mag_zeropoint
+
+
+def mag2flux(mags, mag_zeropoint=0):
+    """
+    Magnitude to flux correction
+    """
+    return 10 ** (-0.4 * (mags + mag_zeropoint))
+
+
+def mag2flux_witherr(mags, magerrs, mag_zeropoint=0):
+    """
+    Magnitude to flux correction, with errors
+    """
+    fluxes = 10 ** (-0.4 * (mags + mag_zeropoint))
+    fluxerrs = fluxes * (10 ** np.abs(0.4 * magerrs) - 1)
+    return fluxes, fluxerrs
