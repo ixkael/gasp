6781a093b83b7756235135a16bf90ba495badde3
Boris Leistedt <boris.leistedt@gmail.com>
2020-10-03 21:59:19 +0100

    improved

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-import numpy
-from sphinx.setup_command import BuildDoc
+from setuptools import setup
 
 version = "0.0.1"
 
-setup(name="gasp", version=version, packages=["gasp"])
+setup(
+    name="gasp",
+    version=version,
+    packages=["gasp"],
+    setup_requires=[
+        "pytest-runner",
+    ],
+    tests_require=[
+        "pytest",
+    ],
+)
