0b139b12c65d015c4bcf73d53b7d33596b610aa5
ixkael <boris.leistedt@gmail.com>
2020-12-23 14:12:20 +0000

    minor edits

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from setuptools import setup
+import os
+import sys
+import re
+
+try:
+    from setuptools import setup
+
+    setup
+except ImportError:
+    from distutils.core import setup
+
+    setup
 
 version = "0.1.0"
 
@@ -12,4 +23,6 @@ setup(
     tests_require=[
         "pytest",
     ],
+    package_data={"gasp": ["data/filters/*par"]},
+    include_package_data=True,
 )
